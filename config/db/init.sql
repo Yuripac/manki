8056f284830cd985e1710718af4d72e53faf2b88
Yuri S Dias <yurimsd94@gmail.com>
2023-07-29 10:58:35 -0300

    fix docker

--- config/db/init.sql
+++ config/db/init.sql
@@ -1,8 +1,10 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
 );
 
+DROP TABLE IF EXISTS cards;
 CREATE TABLE cards (
     id INTEGER PRIMARY KEY,
     sentence TEXT NOT NULL,
